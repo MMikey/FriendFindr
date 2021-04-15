e5bd16ba974f856bb1a4a2607982e4a3d75403a6
MMikey <81487699+MMikey@users.noreply.github.com>
2021-04-15 17:52:28 +0100

    Revert "adding more html"
    This reverts commit f28de56567aae7962702aa2685490907230283b2.

--- welcome.php
+++ welcome.php
@@ -53,7 +53,7 @@ function getRecommendedGroups()
             }
 
     } else {
-        $group_err = "No groups to recommend.. Try selecting some hobbies";
+        //$group_err = "No groups to recommend.. Try selecting some hobbies";
     }
     if (!empty($group_err)) {
         echo '<div class="alert alert-danger">' . $group_err . '</div>';
@@ -67,58 +67,38 @@ function getRecommendedGroups()
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width" />
-    <meta name="description" content="This is a friend finding Application" />
     <title>Welcome</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/wpCSS.css"">
+    <link rel="stylesheet" type="text/css" href="css/FFStylesheet.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
-<section id="nav-bar">
-    <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src="./data/logo.png" /></a>
-            <button
-                    class="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-            >
-                <i class="fa fa-bars"></i>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="welcome.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="groups-page.php">All Groups</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-                            Profile
-                        </a>
-                        <div class="dropdown-menu" style="color:black">
-                            <a class="dropdown-item" href="profile-page.php">My Profile</a>
-                            <a class="dropdown-item" href="update-profile.php">Edit Profile</a>
-                            <a class="dropdown-item" href="reset-password.php">Reset Password</a>
-                            <a class="dropdown-item" href="logout.php">Logout</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About us</a>
-                    </li>
-                </ul>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <!-- Links -->
+    <a class="navbar-brand" href="index.php">FriendFindr</a>
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="welcome.php">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="groups-page.php">All groups</a>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                Profile
+            </a>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="profile-page.php">My Profile</a>
+                <a class="dropdown-item" href="update-profile.php">Edit Profile</a>
+                <a class="dropdown-item" href="reset-password.php">Reset Password</a>
+                <a class="dropdown-item" href="logout.php">Logout</a>
             </div>
-        </div>
-    </nav>
-</section>
+        </li>
+    </ul>
+</nav>
 <h1 class="my-5" style="text-align: center">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to FriendFindr</h1>
 <div class="container">
     <h2>Your groups</h2>
@@ -129,44 +109,11 @@ function getRecommendedGroups()
     <?php getRecommendedGroups(); ?>
 
 </div>
-<!-----sodicla media ------>
-<section id="social-media">
-    <div class="container text-center">
-        <p>FIND US ON SOCIAL MEDIA</p>
-
-        <div class="social-icons">
-            <a href="#"> <img src="./data/fb.png" /> </a>
-            <a href="#"> <img src="./data/ig.png" /> </a>
-            <a href="#"> <img src="./data/ws.jpg" /> </a>
-        </div>
-    </div>
-</section>
-
-<!----Footer Section---->
-<section id="footer">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 footer-box">
-                <img src="./data/logo.png" />
-                <p>
-                    Welcome To FriendFindr:
-                    Join your group and become a part of something
-                </p>
-            </div>
-            <div class="col-md-4 footer-box">
-                <p><b> Contact us </b></p>
-                <p><i class="fa fa-map-marker"></i> MMU, Manchester</p>
-                <p><i class="fa fa-phone"></i>01617959454</p>
-                <p><i class="fa fa-envelope"></i>BlaBlaBla@hotmail.co.uk</p>
-            </div>
-            <div class="col-md-4 footer-box">
-                <p><b> Subscribe </b></p>
-                <input type="email" class="form-control" placeholder="Your Email" />
-                <button type="button" class="btn btn-primary">Contact us</button>
-            </div>
-        </div>
-    </div>
-</section>
+<p>
+    <a href="update-profile.php" class="btn btn-secondary">Update Profile</a>
+    <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+    <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+</p>
 
 </body>
 </html>
